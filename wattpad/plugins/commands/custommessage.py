7964377f1ddcac576ae4c50c33c03ea3247f1378
sagarambati <sagarambati734@gmail.com>
2022-07-15 22:36:17 +0530

    added set custom msg for author command logic - db version

--- wattpad/plugins/commands/custommessage.py
+++ wattpad/plugins/commands/custommessage.py
@@ -28,7 +28,7 @@ async def set_custom_message(ctx:lightbulb.SlashContext) -> None:
 @set_custom_message.child
 @lightbulb.add_checks(lightbulb.checks.has_role_permissions(hikari.Permissions.ADMINISTRATOR)|lightbulb.checks.has_role_permissions(hikari.Permissions.MODERATE_MEMBERS)|lightbulb.checks.has_role_permissions(hikari.Permissions.MANAGE_CHANNELS)|lightbulb.owner_only)
 @lightbulb.option("url","Mention the title/URL of the story",str, required=True)
-@lightbulb.option("message","Your custom message",required=True)
+@lightbulb.option("message","Your custom message for story updates",required=True)
 @lightbulb.command("for-story","set a custom messages for a particular story", auto_defer=True)
 @lightbulb.implements(lightbulb.SlashSubCommand)
 async def set_custom_message_for_story(ctx:lightbulb.SlashContext) -> None:
@@ -43,12 +43,65 @@ async def set_custom_message_for_story(ctx:lightbulb.SlashContext) -> None:
 
         #call the exec
         result= await CustomMessageExec().set_custom_message_for_story(guildId, story_url, message)
+
+        if result.IsSuccess:
+            return_msg= msgs['custom:msg:set:story:success'].format(f"{message}", f"{story_url}")
+            await ctx.respond(embed=hikari.Embed(title=f"{msgs['success']}", description=f"{return_msg}", color=0xFF0000))
+
+        else:
+            logger.error("Error occured in %s.set_custom_message_for_story method for server: %s, story: %s, msg: %s, error: %s", file_prefix, ctx.guild_id, ctx.options.url, ctx.options.message, result.ResultInfo)
+
+            if result.IsInvalidTitle:
+                await ctx.respond(embed=hikari.Embed(title=f"{msgs['error']}", description=f"{msgs['invalid:title']}", color=0xFF0000))
+            
+            elif result.HasMultipleResults:
+                await ctx.respond(embed=hikari.Embed(title=f"{msgs['error']}", description=f"{msgs['story:multiple:title']}", color=0xFF0000))
+
+            else:
+                await ctx.respond(embed=hikari.Embed(title=f"{msgs['unknown:error']}", description=f"{msgs['unknown:error:msg']}", color=0xFF0000))
     
     except Exception as e:
-        logger.fatal("Exception occured in %s. method",exc_info=1)
+        logger.fatal("Exception occured in %s.set_custom_message_for_story method invoked for server: %s, story: %s, msg: %s", file_prefix, ctx.guild_id, ctx.options.url, ctx.options.message,exc_info=1)
         raise e
     
+@set_custom_message.child
+@lightbulb.add_checks(lightbulb.checks.has_role_permissions(hikari.Permissions.ADMINISTRATOR)|lightbulb.checks.has_role_permissions(hikari.Permissions.MODERATE_MEMBERS)|lightbulb.checks.has_role_permissions(hikari.Permissions.MANAGE_CHANNELS)|lightbulb.owner_only)
+@lightbulb.option("url","Mention the title/URL of the Author",str, required=True)
+@lightbulb.option("message","Your custom message for author's announcements",required=True)
+@lightbulb.command("for-announcements","set a custom messages for a particular author's announcements", auto_defer=True)
+@lightbulb.implements(lightbulb.SlashSubCommand)
+async def set_custom_message_for_author(ctx:lightbulb.SlashContext) -> None:
+    try:
+        logger.info("%s.set_custom_message_for_author method invoked for server: %s, author: %s, msg: %s", file_prefix, ctx.guild_id, ctx.options.url, ctx.options.message)
 
+        guildId= str(ctx.guild_id)
+        message= ctx.options.message
+        author_url= ctx.options.url
+
+        msgs= await Config().get_messages("en")
+
+        #call the exec
+        result= await CustomMessageExec().set_custom_message_for_author(guildId, author_url, message)
+
+        if result.IsSuccess:
+            return_msg= msgs['custom:msg:set:author:success'].format(f"{message}", f"{author_url}")
+            await ctx.respond(embed=hikari.Embed(title=f"{msgs['success']}", description=f"{return_msg}", color=0xFF0000))
+
+        else:
+            logger.error("Error occured in %s.set_custom_message_for_author method for server: %s, author: %s, msg: %s, error: %s", file_prefix, ctx.guild_id, ctx.options.url, ctx.options.message, result.ResultInfo)
+
+            if result.IsInvalidTitle:
+                await ctx.respond(embed=hikari.Embed(title=f"{msgs['error']}", description=f"{msgs['invalid:title']}", color=0xFF0000))
+            
+            elif result.HasMultipleResults:
+                await ctx.respond(embed=hikari.Embed(title=f"{msgs['error']}", description=f"{msgs['author:multiple:title']}", color=0xFF0000))
+
+            else:
+                await ctx.respond(embed=hikari.Embed(title=f"{msgs['unknown:error']}", description=f"{msgs['unknown:error:msg']}", color=0xFF0000))
+    
+    except Exception as e:
+        logger.fatal("Exception occured in %s.set_custom_message_for_author method invoked for server: %s, author: %s, msg: %s", file_prefix, ctx.guild_id, ctx.options.url, ctx.options.message,exc_info=1)
+        raise e
 
 
 
--- wattpad/pluginsexecution/commandsexec/custommessageexec.py
+++ wattpad/pluginsexecution/commandsexec/custommessageexec.py
@@ -1,11 +1,12 @@
 from datetime import datetime
+from typing import List
 from wattpad.db.models.custommsg import CustomMsg
 from wattpad.db.repository.authorrepo import AuthorRepo
 from wattpad.db.repository.channelrepo import ChannelRepo
 from wattpad.db.repository.serverrepo import ServerRepo
 from wattpad.db.repository.storyrepo import StoryRepo
 from wattpad.logger.baselogger import BaseLogger
-from wattpad.meta.models.result import Result
+from wattpad.meta.models.result import Result, ResultCustomChannelSet
 from wattpad.db.repository.custommsgrepo import CustomMsgrepo
 from wattpad.meta.models.enum import CustomMsgType
 
@@ -20,7 +21,7 @@ class CustomMessageExec:
         self.customMsgRepo= CustomMsgrepo()
         self.prefix= "wattpad.com"
 
-    async def set_custom_message_for_story(self, guildid: str, storyurl:str, message:str) -> Result:
+    async def set_custom_message_for_story(self, guildid: str, storyurl:str, message:str) -> ResultCustomChannelSet:
         try:
             self.logger.info("%s.set_custom_message_for_story method invoked for server: %s, story: %s, msg: %s", self.file_prefix, guildid, storyurl, message)
 
@@ -30,23 +31,52 @@ class CustomMessageExec:
                 story_urls= await self.__get_story_url_from_title(storyurl, guildid)
 
             if not story_urls:
-                return Result(False, "No story found with the title", IsInvalidTitle=True)
+                return ResultCustomChannelSet(False, "No story found with the title", IsInvalidTitle=True)
 
             else:
                 if len(story_urls) > 1:
-                    return Result(False, "Multiple stories found with this title", HasMultipleResults=True)
+                    return ResultCustomChannelSet(False, "Multiple stories found with this title", HasMultipleResults=True)
 
                 else:
                     result= await self.__set_custom_message(guildid, story_urls, message, isstory=True)
 
                     if result.IsSuccess:
-                        return Result(True, "success")
+                        return ResultCustomChannelSet(True, "success")
 
                     
-            return Result(False, "Unknown error", UnknownError=True)
+            return ResultCustomChannelSet(False, "Unknown error", UnknownError=True)
         
         except Exception as e:
             self.logger.fatal("Exception occured in %s.set_custom_message_for_story method invoked for server: %s, story: %s, msg: %s", self.file_prefix, guildid, storyurl, message,exc_info=1)
+            raise e
+
+    async def set_custom_message_for_author(self, guildid: str, authorurl:str, message:str) -> ResultCustomChannelSet:
+        try:
+            self.logger.info("%s.set_custom_message_for_author method invoked for server: %s, author: %s, msg: %s", self.file_prefix, guildid, authorurl, message)
+
+            author_urls=""            
+
+            if self.prefix not in authorurl:
+                author_urls= await self.__get_author_url_from_title(authorurl, guildid)
+
+            if not author_urls:
+                return ResultCustomChannelSet(False, "No Author found with the name", IsInvalidTitle=True)
+
+            else:
+                if len(author_urls) > 1:
+                    return ResultCustomChannelSet(False, "Multiple authors found with this name", HasMultipleResults=True)
+
+                else:
+                    result= await self.__set_custom_message(guildid, author_urls, message, isauthor=True, isstory=False)
+
+                    if result.IsSuccess:
+                        return ResultCustomChannelSet(True, "success")
+
+                    
+            return ResultCustomChannelSet(False, "Unknown error", UnknownError=True)
+        
+        except Exception as e:
+            self.logger.fatal("Exception occured in %s.set_custom_message_for_author method invoked for author: %s, story: %s, msg: %s", self.file_prefix, guildid, authorurl, message,exc_info=1)
             raise e
 
     async def __set_custom_message(self, guildid: str, url: str, message: str, isstory:bool= True, isauthor:bool= False) -> Result:
@@ -56,18 +86,32 @@ class CustomMessageExec:
             serverid= await self.serverRepo.get_serverid_from_server(guildid)
 
             if serverid:
-                #get the story id
-                storyid= await self.storyRepo.get_story_id_from_server_and_url(url, serverid, 1)
+                if isauthor:
+                    authorid= await self.authorRepo.get_author_id_from_server_and_url(url, serverid, 1)
 
-                if storyid:
-                    custommsg= CustomMsg(Type=CustomMsgType.Story, StoryId=storyid, AuthorId=0, IsActive=1, Message=message)
-                    result= await self.customMsgRepo.insert_custom_msg_data(custommsg)
-                    if result:
-                        return Result(True, "success")
+                    if authorid:
+                        custommsg= CustomMsg(Type=CustomMsgType.Author, StoryId=0, AuthorId=authorid, IsActive=1, Message=message)
+                        result= await self.customMsgRepo.insert_custom_msg_data(custommsg)
+                        if result:
+                            return Result(True, "success")
+                        else:
+                            return Result(False, "Error while inserting the custom msg data")
                     else:
-                        return Result(False, "Error while inserting the custom msg data")
+                        return Result(False, "Error while fetching the author id") #return story not found
+
                 else:
-                    return Result(False, "Error while fetching the story id") #return story not found
+                    #get the story id
+                    storyid= await self.storyRepo.get_story_id_from_server_and_url(url, serverid, 1)
+
+                    if storyid:
+                        custommsg= CustomMsg(Type=CustomMsgType.Story, StoryId=storyid, AuthorId=0, IsActive=1, Message=message)
+                        result= await self.customMsgRepo.insert_custom_msg_data(custommsg)
+                        if result:
+                            return Result(True, "success")
+                        else:
+                            return Result(False, "Error while inserting the custom msg data")
+                    else:
+                        return Result(False, "Error while fetching the story id") #return story not found
             
             else:
                 return Result(False, "Error while fetching the server id")
