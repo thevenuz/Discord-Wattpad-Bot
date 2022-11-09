fbdd3cfd470d55b1bd8857b0247f27cfb98e875e
sagarambati <sagarambati734@gmail.com>
2022-11-09 23:20:22 +0530

    add utils for json version rewrite

---
+++ wattpad/utils/config.py
@@ -0,0 +1,24 @@
+from wattpad.logger.baselogger import BaseLogger
+from typing import Dict
+import aiofiles
+import json
+
+class Config:
+    def __init__(self) -> None:
+        self.file_prefix = "wattpad.utils.config"
+        self.logger = BaseLogger().loggger_init()
+
+    async def get_messages(self, language: str = "en") -> Dict:
+        try:
+            self.logger.info("%s.get_messages method invoked for language: %s", self.file_prefix, language)
+
+            filepath="lang/en.json" #TODO: make this dynamic
+
+            async with aiofiles.open(filepath, mode="r") as f:
+                result = json.loads(await f.read())
+
+            return result
+
+        except Exception as e:
+            self.logger.fatal("Exception occured in %s.get_messages method for language: %s", self.file_prefix, language, exc_info=1)
+            raise e
---
+++ wattpad/utils/jsonutil.py
@@ -0,0 +1,48 @@
+from wattpad.logger.baselogger import BaseLogger
+from typing import Dict
+import os
+import aiofiles
+import json
+
+
+class JsonUtil:
+    def __init__(self) -> None:
+        self.file_prefix = "wattpad.utils.jsonutil"
+        self.logger = BaseLogger().loggger_init()
+
+    async def get_file_path(self, file: str) -> str:
+        """
+            returns absolute file path
+        """
+        try:
+            self.logger.info("%s.get_file_path method invoked for file: %s", self.file_prefix, file)
+
+            if file is None:
+                raise TypeError("empty file")
+
+            filePath = os.path.join(os.path.dirname(
+                os.path.dirname(__file__)), "config", file)
+
+            return filePath
+
+        except Exception as e:
+            self.logger.fatal("Exception occured in %s.get_file_path method for file: %s", self.file_prefix, file, exc_info=1)
+            raise e
+
+    async def read_from_json(self, file: str) -> Dict:
+        """
+            read json data from a given json file in config folder
+        """
+        try:
+            self.logger.info("%s.read_from_json method invoked for file: %s", self.file_prefix, file)
+
+            filePath = await self.get_file_path(file)
+
+            async with aiofiles.open(filePath, mode="r") as f:
+                result = json.loads(await f.read())
+
+            return result
+
+        except Exception as e:
+            self.logger.info("Exception occured in %s.read_from_json for file: %s", self.file_prefix, file, exc_info=1)
+            raise e
