df8950717efda1aa62adf067b130a3d6b0b32b4d
sagarambati <sagarambati734@gmail.com>
2022-12-08 23:04:34 +0530

    add set custom msg for author command

--- wattpad/plugins/commands/custommessage.py
+++ wattpad/plugins/commands/custommessage.py
@@ -71,4 +71,52 @@ async def set_custom_message_for_story(ctx:lightbulb.SlashContext) -> None:
         logger.fatal("Exception occured in %s.set_custom_message_for_story method invoked for server: %s, story: %s, msg: %s", filePrefix, ctx.guild_id, ctx.options.url, ctx.options.message, exc_info=1)
         raise e
     
+@set_custom_message.child
+@lightbulb.add_checks(lightbulb.checks.has_role_permissions(hikari.Permissions.ADMINISTRATOR)|lightbulb.checks.has_role_permissions(hikari.Permissions.MODERATE_MEMBERS)|lightbulb.checks.has_role_permissions(hikari.Permissions.MANAGE_CHANNELS)|lightbulb.owner_only)
+@lightbulb.option("url","Mention the title/URL of the Author",str)
+@lightbulb.option("message","Your custom message for author's announcements",required=True)
+@lightbulb.command("for-announcements","set a custom messages for a particular author's announcements", auto_defer=True)
+@lightbulb.implements(lightbulb.SlashSubCommand)
+async def set_custom_message_for_author(ctx:lightbulb.SlashContext) -> None:
+    try:
+        logger.info("%s.set_custom_message_for_author method invoked for server: %s, author: %s, msg: %s", filePrefix, ctx.guild_id, ctx.options.url, ctx.options.message)
 
+        guildId= str(ctx.guild_id)
+        message= ctx.options.message
+        storyUrl= ctx.options.url
+
+        config = Config()
+
+        language = await config.get_language(guildId)
+        msgs = await config.get_messages(language)
+
+        #call the implementation
+        result = await CustomMessageImpl().set_custom_message_for_author(guildId, storyUrl, message)
+
+        if result.IsSuccess:
+            if storyUrl:
+                response = msgs['custom:msg:set:author:success'].format(f"{message}", f"{storyUrl}")
+                await ctx.respond(embed=hikari.Embed(title=f"{msgs['success']}", description=f"{response}", color=0Xff500a))
+
+            else:
+                await ctx.respond(embed=hikari.Embed(title=f"{msgs['success']}", description=f"{msgs['custom:msg:set:author:category:success']}", color=0Xff500a))
+
+        else:
+            logger.error("Error occured in %s.set_custom_message_for_author method for server: %s, author: %s, msg: %s, error: %s", filePrefix, ctx.guild_id, ctx.options.url, ctx.options.message, result.ResultInfo)
+
+            if result.NoAuthorNameFound:
+                await ctx.respond(embed=hikari.Embed(title=f"{msgs['error']}", description=f"{msgs['invalid:title']}", color=0xFF0000))
+
+            elif result.NoAuthorFound:
+                    await ctx.respond(embed=hikari.Embed(title=f"{msgs['error']}", description=f"{msgs['unfollow:author:not:following']}", color=0xFF0000))
+            
+            elif result.MultipleAuthorsFound:
+                await ctx.respond(embed=hikari.Embed(title=f"{msgs['error']}", description=f"{msgs['author:multiple:title']}", color=0xFF0000))
+
+            else:
+                await ctx.respond(embed=hikari.Embed(title=f"{msgs['unknown:error']}", description=f"{msgs['unknown:error:msg']}", color=0xFF0000))
+
+    except Exception as e:
+        logger.fatal("Exception occured in %s.set_custom_message_for_author method invoked for server: %s, author: %s, msg: %s", filePrefix, ctx.guild_id, ctx.options.url, ctx.options.message,exc_info=1)
+        raise e
+    
--- wattpad/pluginsimpl/commands/custommessageimpl.py
+++ wattpad/pluginsimpl/commands/custommessageimpl.py
@@ -83,3 +83,77 @@ class CustomMessageImpl:
             self.logger.fatal("Exception occured in %s.set_custom_message_for_story method invoked for server: %s, story: %s, msg: %s", self.filePrefix, guildId, url, message, exc_info=1)
             raise e
         
+    async def set_custom_message_for_author(self, guildId: str, url: str = "", message: str = "") -> ResultSetCustomChannel:
+        try:
+            self.logger.info("%s.set_custom_message_for_author method invoked for server: %s, author: %s, msg: %s", self.filePrefix, guildId, url, message)
+
+            dataUtil = DataUtil()
+            authorUrls = []
+
+            #if there is a url, custom msg needs to be setup for individual authors
+            if url:
+                #load authors
+                authors = await dataUtil.get_authors()
+
+                filterredAuthors = dict(filter(lambda x: x[0] == guildId, authors.items()))
+
+                if self.filePrefix not in url:
+                    authorUrls = [author["url"] for author in filterredAuthors[guildId] if self.prefix in author["url"]]
+
+                else:
+                    authorUrls.append(url)
+                
+                #not following any author with that name/url
+                if not authorUrls:
+                    if self.prefix in url:
+                        return ResultSetCustomChannel(False, "No author found", NoAuthorFound= True)
+
+                    else:
+                        return ResultSetCustomChannel(False, "No author found with the name", NoAuthorNameFound= True)
+
+                else:
+                    if len(authorUrls) > 1:
+                        return ResultSetCustomChannel(False, "Mutliple stories found with this name", MultipleAuthorsFound= True)
+
+                    else:
+                        for guild, authorlist in filterredAuthors:
+                            for author in authorlist:
+                                if authorUrls[0] == author["url"]:
+                                    author["CustomMsg"] = message
+
+                                    #update the orginal stories json data
+                                    authors[guildId] = author
+
+                                    result = await dataUtil.update_authors(authors)
+
+                                    authorName = await WattpadUtil().get_author_name(url)
+
+                                    if result:
+                                        return ResultSetCustomChannel(True, "custom msg set success", AuthorName= authorName)
+
+                                    else:
+                                        return ResultSetCustomChannel(False, "Error occured while updating authors", UnknownError= True)
+
+            #if there is no url set custom msg for whole author category
+            else:
+                #get messages
+                messages = await dataUtil.get_messages()
+
+                messages[guildId]["announcement"] = message
+
+                result = await dataUtil.update_messages(messages)
+
+                authorName = await WattpadUtil().get_author_name(url)
+
+                if result:
+                    return ResultSetCustomChannel(True, "custom msg set success", AuthorName= authorName)
+
+                else:
+                    return ResultSetCustomChannel(False, "Error occured while updating authors", UnknownError= True)
+
+            return ResultSetCustomChannel(False, "unknown error", UnknownError= True)
+
+        except Exception as e:
+            self.logger.fatal("Exception occured in %s.set_custom_message_for_author method invoked for author: %s, story: %s, msg: %s", self.filePrefix, guildId, url, message, exc_info=1)
+            raise e
+        
