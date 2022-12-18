c1e2652977afb7f32c536883de221f2fdb9ccdc3
sagarambati <sagarambati734@gmail.com>
2022-12-18 21:59:07 +0530

    update logger to rotating logger

--- wattpad/logger/baselogger.py
+++ wattpad/logger/baselogger.py
@@ -1,4 +1,7 @@
 import logging
+import os
+from pathlib import Path
+import json
 from logging.handlers import TimedRotatingFileHandler
 from datetime import datetime
 
@@ -12,11 +15,28 @@ class BaseLogger:
             if (logger.hasHandlers()):
                 logger.handlers.clear()
 
-            logger.setLevel(logging.DEBUG)
+            rootDirectory = os.path.dirname(__file__)
+            configDirectory = Path(rootDirectory).parents[1]
+
+            filePath = os.path.join(configDirectory, "config", "settings.json")
+
+            with open(filePath) as f:
+                result = json.load(f)
+            
+            logginglevel = result["LogLevel"]
+
+            if not logginglevel:
+                logginglevel = "ERROR"
+
+            loglevel = logging.getLevelName(logginglevel)
+            
+            logger.setLevel(loglevel)
 
             formatter=logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
 
-            logname = f"logs\log_{datetime.utcnow().strftime('%Y%m%d')}.txt"
+            filePath = os.path.join(configDirectory, "logs")
+
+            logname = f"{filePath}\log_{datetime.utcnow().strftime('%Y%m%d')}.txt"
             handler = TimedRotatingFileHandler(logname, when="midnight", interval=1)
             handler.suffix = "%Y%m%d"
             handler.setFormatter(formatter)
